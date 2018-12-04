f84e7c08964d230de9a1ae45ed00fb7b6c863f3e
sist <narsha.kim@gmail.com>
2018-12-04 17:50:19 +0900

    HireDaoImpl

---
+++ GJ/src/main/java/com/sist/gj/dao/HireDaoImpl.java
@@ -0,0 +1,37 @@
+package com.sist.gj.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.sist.gj.vo.HireVO;
+
+@Repository
+public class HireDaoImpl implements HireDao {
+
+	@Override
+	public int create(HireVO hireVO) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(HireVO hireVO) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<HireVO> selectAll() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<HireVO> select(HireVO hireVO) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
