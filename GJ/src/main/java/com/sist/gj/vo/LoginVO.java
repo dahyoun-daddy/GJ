229207c5da31a96e29efbea5009c094e799b7c71
sist1 <sist1@sist>
2018-12-04 14:36:32 +0900

    JHJH

---
+++ GJ/src/main/java/com/sist/gj/vo/LoginVO.java
@@ -0,0 +1,22 @@
+package com.sist.gj.vo;
+
+public class LoginVO {
+	
+	private String  userId		      ;//사용자 아이디
+	private String  userPasswd        ;//사용자 비밀번호
+	private String  userNick          ;//사용자 닉네임or기업명
+	private String  userName          ;//사용자 이름or대표이름
+	private String  userAdd  	      ;//사용자 주소
+	private String  userPassQ	      ;//비밀번호찾기질문
+	private String  userPassA	      ;//비밀번호찾기답
+	private String  userPhone	      ;//전화번호
+	private String  userLevel	      ;//계정구분
+	private String  enterSalay	      ;//기업 연매출액
+	private String  enterCnt	      ;//기업 사원수
+	private String  enterHiredate	  ;//기업 설립일
+	private String  regId			  ;//등록자
+	private String  regDt			  ;//등록일
+	private String  modId			  ;//수정자
+	private String  modDt			  ;//수정일
+	
+}
