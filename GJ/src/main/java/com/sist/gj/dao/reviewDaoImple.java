d2342b3a9927675973b85612a2df28bbc478da39
sist <dkdk6689@hanmail.net>
2018-12-04 16:23:20 +0900

    test

--- GJ/src/main/java/com/sist/gj/dao/reviewDaoImple.java
+++ GJ/src/main/java/com/sist/gj/dao/reviewDaoImple.java
@@ -10,7 +10,7 @@ public class reviewDaoImple implements reviewDao {
 
 	@Override
 	public int update(ReviewVO reviewVO) throws SQLException {
-		// TODO Auto-generated method stub 되겠지?
+		// TODO Auto-generated method stub 되겠지??
 		return 0;
 	}
 
