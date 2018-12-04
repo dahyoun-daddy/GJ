57b1dc3617c06fe0a8e782d53a4d00eef83ea160
sist <boondll@hanmail.net>
2018-12-04 18:24:40 +0900

    mypageDao+imple

---
+++ GJ/src/main/java/com/sist/gj/dao/MypageDaoImple.java
@@ -0,0 +1,110 @@
+package com.sist.gj.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.sist.gj.vo.ApplyVO;
+import com.sist.gj.vo.CvFormVO;
+import com.sist.gj.vo.DTO;
+import com.sist.gj.vo.LicenseVO;
+import com.sist.gj.vo.PictureVO;
+
+public class MypageDaoImple implements MypageDao {
+
+	@Override
+	public int addPic(PictureVO pictureVO) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updatePic(PictureVO pictureVO) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deletePic(PictureVO pictureVO) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public CvFormVO selectCv(CvFormVO cvFormVO) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateCv(CvFormVO cvFormVO) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int addCv(CvFormVO cvFormVO) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<CvFormVO> retrieveCv() throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int addLic(LicenseVO licenseVO) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteLic(LicenseVO licenseVO) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public LicenseVO selectLic(LicenseVO licenseVO) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ApplyVO> retrieveApply() throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int deletePic(ApplyVO applyVO) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public CvFormVO selectUserInfo(DTO DTO) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CvFormVO selectCompInfo(DTO DTO) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int deleteUser(DTO DTO) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateUSer(DTO DTO) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
