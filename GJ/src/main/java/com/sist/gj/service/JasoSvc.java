0425a0c8dd36838d5f590fa613350eaf0b50e636
sist <1seoho1@naver.com>
2018-12-05 14:15:26 +0900

    Service파일 작성, 코드정리

--- GJ/src/main/java/com/sist/gj/service/JasoSvc.java
+++ GJ/src/main/java/com/sist/gj/service/JasoSvc.java
@@ -1,5 +1,19 @@
 package com.sist.gj.service;
 
-public interface JasoSvc {
+import java.sql.SQLException;
+import java.util.List;
 
+import com.sist.gj.vo.JasoVO;
+import com.sist.gj.vo.SearchVO;
+
+public interface JasoSvc {
+	int update(JasoVO jasoVO) throws SQLException;
+	int delete(JasoVO jasoVO) throws SQLException;
+	//추가
+	int add(JasoVO jasoVO);
+	
+	List<JasoVO> doRetrieve(SearchVO searchVO) throws ClassNotFoundException, SQLException;
+
+	//조회
+	JasoVO select(JasoVO jasoVO) throws ClassNotFoundException, SQLException;
 }
---
+++ GJ/src/main/java/com/sist/gj/service/JasoSvcImple.java
@@ -0,0 +1,44 @@
+package com.sist.gj.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sist.gj.dao.JasoDao;
+import com.sist.gj.vo.JasoVO;
+import com.sist.gj.vo.SearchVO;
+
+@Service
+public class JasoSvcImple implements JasoSvc {
+
+	@Autowired
+	private JasoDao jasoDao;
+	
+	@Override
+	public int update(JasoVO jasoVO) throws SQLException {
+		return jasoDao.update(jasoVO);
+	}
+
+	@Override
+	public int delete(JasoVO jasoVO) throws SQLException {
+		return jasoDao.delete(jasoVO);
+	}
+
+	@Override
+	public int add(JasoVO jasoVO) {
+		return jasoDao.add(jasoVO);
+	}
+
+	@Override
+	public List<JasoVO> doRetrieve(SearchVO searchVO) throws ClassNotFoundException, SQLException {
+		return jasoDao.doRetrieve(searchVO);
+	}
+
+	@Override
+	public JasoVO select(JasoVO jasoVO) throws ClassNotFoundException, SQLException {
+		return jasoDao.select(jasoVO);
+	}
+
+}
