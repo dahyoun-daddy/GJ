1cbbc1383fde998096fdf3944bfc4170d8327418
sist <1seoho1@naver.com>
2018-12-05 17:21:19 +0900

    xml수정, 컨트롤러연동

--- GJ/src/main/java/com/sist/gj/control/JasoController.java
+++ GJ/src/main/java/com/sist/gj/control/JasoController.java
@@ -1,20 +1,52 @@
 package com.sist.gj.control;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sist.gj.service.JasoCommentSvc;
 import com.sist.gj.service.JasoSvc;
+import com.sist.gj.vo.JasoVO;
+import com.sist.gj.vo.SearchVO;
 
 @Controller
 public class JasoController {
 	Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	private static final String VIEW_NAME="jaso/jasoList";
 	
 	@Autowired
 	private JasoSvc jasoSvc;
 	@Autowired
 	private JasoCommentSvc jasoCSvc;
 	
+	@RequestMapping(value="/jaso/jaso.do")
+	public String doRetrieve(@ModelAttribute SearchVO invo, Model model) throws ClassNotFoundException, SQLException {
+		log.debug("search : "+invo);
+		
+		if(invo.getPageSize() == 0) {
+			invo.setPageSize(10);
+		}
+		if(invo.getPageNum() == 0){
+			invo.setPageNum(1);
+		}
+		if(null == invo.getSearchDiv()) {
+			invo.setSearchDiv("");
+		}
+		if(null == invo.getSearchDiv()) {
+			invo.setSearchDiv("");
+		}
+		
+		List<JasoVO> list = jasoSvc.doRetrieve(invo);
+		model.addAttribute("list",list);
+		
+		return VIEW_NAME;
+	}
 }
