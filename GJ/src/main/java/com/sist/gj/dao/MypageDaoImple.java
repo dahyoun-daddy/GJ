bd3d386a1101730b238f98b975a333841258001a
sist <boondll@hanmail.net>
2018-12-07 16:01:02 +0900

    MypageSvc, MypageSvcImple complete

--- GJ/src/main/java/com/sist/gj/dao/MypageDaoImple.java
+++ GJ/src/main/java/com/sist/gj/dao/MypageDaoImple.java
@@ -3,6 +3,12 @@ package com.sist.gj.dao;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.sist.gj.vo.ApplyVO;
 import com.sist.gj.vo.CvFormVO;
 import com.sist.gj.vo.DTO;
@@ -10,44 +16,84 @@ import com.sist.gj.vo.JasoVO;
 import com.sist.gj.vo.LicenseVO;
 import com.sist.gj.vo.PictureVO;
 import com.sist.gj.vo.SearchVO;
+import com.sist.gj.vo.UserInfoVO;
 import com.sist.gj.vo.UserVO;
 
+@Repository
 public class MypageDaoImple implements MypageDao {
+	
+	Logger log = LoggerFactory.getLogger(this.getClass());
+	private final String NAMESPACE = "com.sist.gj.mappers.userMypage";
 
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	public MypageDaoImple() {}
+	
 	@Override
 	public int addPic(PictureVO pictureVO) {
-		// TODO Auto-generated method stub
-		return 0;
+		String statement = NAMESPACE+".addPic";
+		log.debug("sql statement : "+statement);
+		log.debug("param : "+pictureVO);
+		int flag = sqlSession.insert(statement, pictureVO);
+		log.debug("result : "+flag);
+				
+		return flag;
 	}
 
 	@Override
 	public int deletePic(PictureVO pictureVO) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		String statement = NAMESPACE+".deletePic";
+		log.debug("sql statement : "+statement);
+		log.debug("param : "+pictureVO);
+		int flag = sqlSession.delete(statement, pictureVO);
+		log.debug("result : "+flag);
+				
+		return flag;
 	}
 
 	@Override
 	public CvFormVO selectCv(CvFormVO cvFormVO) throws ClassNotFoundException, SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		String statement = NAMESPACE+".selectCv";
+		log.debug("sql statement : "+statement);
+		log.debug("param : "+cvFormVO);
+		CvFormVO outVO = sqlSession.selectOne(statement, cvFormVO);
+		log.debug("result : "+outVO);
+		
+		return outVO;
 	}
 
 	@Override
 	public int updateCv(CvFormVO cvFormVO) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		String statement = NAMESPACE+".updateCv";
+		log.debug("sql statement : "+statement);
+		log.debug("param : "+cvFormVO);
+		int flag = sqlSession.update(statement, cvFormVO);
+		log.debug("result : "+flag);
+				
+		return flag;
 	}
 
 	@Override
 	public int addCv(CvFormVO cvFormVO) {
-		// TODO Auto-generated method stub
-		return 0;
+		String statement = NAMESPACE+".addCv";
+		log.debug("sql statement : "+statement);
+		log.debug("param : "+cvFormVO);
+		int flag = sqlSession.insert(statement, cvFormVO);
+		log.debug("result : "+flag);
+				
+		return flag;
 	}
 
 	@Override
 	public List<CvFormVO> retrieveCv(SearchVO searchVO) throws ClassNotFoundException, SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		String statement = NAMESPACE+".retrieveCv";
+		log.debug("sql statement : "+statement);
+		log.debug("param : "+searchVO);
+		List<CvFormVO> list = sqlSession.selectList(statement, searchVO);
+		log.debug("result : "+list);
+		
+		return list;
 	}
 
 	@Override
@@ -82,7 +128,7 @@ public class MypageDaoImple implements MypageDao {
 	}
 
 	@Override
-	public int addApply(ApplyVO liceapplyVOnseVO) {
+	public int addApply(ApplyVO applyVO) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
@@ -106,25 +152,25 @@ public class MypageDaoImple implements MypageDao {
 	}
 
 	@Override
-	public UserVO selectUserInfo(UserVO userVO, CvFormVO cvFormVO) throws ClassNotFoundException, SQLException {
+	public UserVO selectUserInfo(UserInfoVO userInfoVO) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public UserVO selectCompInfo(UserVO userVO) throws ClassNotFoundException, SQLException {
+	public UserVO selectCompInfo(UserInfoVO userInfoVO) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public int deleteUser(UserVO DTO) throws SQLException {
+	public int deleteUser(UserVO userVO) throws SQLException {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public int updateUSer(UserVO DTO) throws SQLException {
+	public int updateUser(UserVO userVO) throws SQLException{
 		// TODO Auto-generated method stub
 		return 0;
 	}
