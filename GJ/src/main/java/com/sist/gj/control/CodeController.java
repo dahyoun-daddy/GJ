971eecf57badba4a7a6bf2b08e310e8cece74f47
sist <1seoho1@naver.com>
2018-12-11 17:21:30 +0900

    코드컨트롤러 완성

--- GJ/src/main/java/com/sist/gj/control/CodeController.java
+++ GJ/src/main/java/com/sist/gj/control/CodeController.java
@@ -1,11 +1,17 @@
 package com.sist.gj.control;
 
+import java.sql.SQLException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.sist.gj.service.CodeSvc;
+import com.sist.gj.vo.CodeVO;
 
 
 @Controller
@@ -14,5 +20,11 @@ public class CodeController {
 	
 	@Autowired
 	private CodeSvc codeSvc;
- 
+	
+	@RequestMapping(value="/code/select_one.do",method=RequestMethod.GET)
+	public String do_retrieve(CodeVO dto,Model model) throws ClassNotFoundException, SQLException {
+		log.debug("do_select param : "+dto);
+		model.addAttribute("list",codeSvc.doRetrieve(dto));
+		return "code/code";
+	}
 }
